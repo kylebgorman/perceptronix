1923eb09aaea9af88f2787b2c497b834b41b2803
kylebgorman <kylebgorman@gmail.com>
2020-01-01 21:19:20 -0600

    Version bump

--- lib/setup.py
+++ lib/setup.py
@@ -19,7 +19,7 @@ extension = Extension(
 
 setup(
     name="Perceptronix",
-    version="0.6",
+    version="0.7",
     author="Kyle Gorman",
     author_email="kylebgorman@gmail.com",
     install_requires=["Cython >= 0.29"],
