df93eadb7b2584e91b4c95fde4e142fb9e7cd105
kylebgorman <kylebgorman@gmail.com>
2020-01-01 22:14:00 -0600

    Fixes setup typo

--- setup.py
+++ setup.py
@@ -17,12 +17,28 @@ extension = Extension(
     extra_compile_args=["-std=c++11", "-funsigned-char"],
 )
 
+
+__version__ = "0.7"
+
+
 setup(
-    name="Perceptronix",
     name="perceptronix",
-    version="0.7",
+    version=__version__,
     author="Kyle Gorman",
     author_email="kylebgorman@gmail.com",
+    description="Perceptron classifiers",
+    keywords=[
+        "computational linguistics",
+        "morphology",
+        "natural language processing",
+        "phonology",
+        "phonetics",
+        "speech",
+        "language",
+    ],
+    license="Apache 2.0",
+    python_requires=">=3.6",
+    zip_safe=False,
     install_requires=["Cython >= 0.29"],
     ext_modules=cythonize([extension]),
 )
